a6e4d9d740e1a29c676ab7107aa20760b0dc9b13
m1596284 <m1596284@gmail.com>
2024-08-30 15:07:20 +0800

    refactor: Update print statements in ChatConsumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -6,7 +6,7 @@ from channels.generic.websocket import WebsocketConsumer
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        print("有人來連接")
+        print("ChatConsumer connect")   
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
 
@@ -19,7 +19,7 @@ class ChatConsumer(WebsocketConsumer):
         self.accept()
 
     def disconnect(self, close_code):
-        print("客戶端斷開")
+        print("ChatConsumer disconnect")
         # Leave room group
         print("room_group_name-->", self.room_group_name)
         async_to_sync(self.channel_layer.group_discard)(
@@ -28,7 +28,7 @@ class ChatConsumer(WebsocketConsumer):
 
     # Receive message from WebSocket
     def receive(self, text_data):
-        print("接收瀏覽器的訊息-->", text_data)
+        print("ChatConsumer receive")
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
@@ -40,7 +40,7 @@ class ChatConsumer(WebsocketConsumer):
 
     # Receive message from room group
     def chat_message(self, event):
-        print("接收群組的訊息-->", event)
+        print("ChatConsumer chat_message")
         message = event["message"]
 
         # Send message to WebSocket
